9c9f4fa71c9f40e91832ee111737e0456f9187c3
paulcaron <paul.caron@polytechnique.edu>
2020-06-02 23:05:24 +0200

    Update train.py

--- train.py
+++ train.py
@@ -9,18 +9,34 @@ from utils import PCamDataset
 from resnet_binary_classifier import Resnet_Binary_Classifier, Resnet_Binary_Classifier_Sequential_Unfreeze
 import os
 
+from torch.utils.tensorboard import SummaryWriter
+
+from tqdm import tqdm
+
+from sklearn.metrics import roc_curve, auc
+from sklearn.metrics import roc_auc_score
+import matplotlib.pyplot as plt
+
 ROOT_DIR = os.getcwd().replace("\\", "/")
 LEARNING_RATE = 5e-3
 NUM_EPOCHS = 30
-BATCH_SIZE = 50
-SIZE_TRAIN_DATASET = 50 # To train on small dataset
-SIZE_VAL_DATASET = 50
+BATCH_SIZE = 256
+EVALUATE_EVERY = 50000
+#SIZE_TRAIN_DATASET = 64 # To train on small dataset
+#SIZE_VAL_DATASET = 64
+SIZE_TRAIN_DATASET, SIZE_VAL_DATASET = None, None
 L2_WD = 1e-5
 #LR_SCHEDULER_T_MAX = 10
 #LR_SCHEDULER_ETA_MIN = 1e-5
 
+# TENSORBOARD SETUP
+save_dir = "./save/"
+SAVE_MODEL_PATH = save_dir + "train_resnet_sequential_unfreeze_best" + np.datetime64("now").astype(str).replace('-', '').replace(':', '')
+name = "train_resnet_sequential_unfreeze_" + np.datetime64("now").astype(str).replace('-', '').replace(':', '')
+writer = SummaryWriter(save_dir+name)
+
+# DEVICE SETUP
 USE_GPU = True
-#dtype = torch.float32
 
 if USE_GPU and torch.cuda.is_available():
     device = torch.device('cuda')
@@ -66,7 +82,6 @@ def main():
                         shuffle=True)
     
     model = Resnet_Binary_Classifier()
-    #model = Resnet_Binary_Classifier_Sequential_Unfreeze()
     
     if device.type == 'cuda':
         model.cuda()
@@ -77,20 +92,55 @@ def main():
     #                                                 eta_min=LR_SCHEDULER_ETA_MIN)
     
     epoch = 0
+    n_iter = 0
+    best_val_loss = float("inf")
     while epoch != NUM_EPOCHS:
         epoch += 1
-        for t, (x, y) in enumerate(loader_train):
-            x = x.to(device=device)
-            y = y.to(device=device)
-            model.train()
-            scores = model(x)
-            criterion = torch.nn.BCEWithLogitsLoss()
-            loss = criterion(scores, y)
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-        print('Epoch %d, loss = %.4f' % (epoch, loss.item()))
+        print("Starting epoch {epoch}".format(epoch=epoch))
+        with tqdm(total=train_dataset.__len__()) as progress_bar:
+            for t, (x, y) in enumerate(loader_train):
+                n_iter += BATCH_SIZE
+                x = x.to(device=device)
+                y = y.to(device=device)
+                model.train()
+                scores = model(x)
+                criterion = torch.nn.BCEWithLogitsLoss()
+                loss = criterion(scores, y)
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+                
+                # TENSORBOARD
+                # no smoothing: running_loss
+                loss_val  = loss.item()
+                walltime = (epoch-1)*train_dataset.__len__() + t*BATCH_SIZE
+                writer.add_scalar('train/BCE', loss_val, walltime)
+                
+                # Update progress bar
+                progress_bar.update(BATCH_SIZE)
+                progress_bar.set_postfix(epoch=epoch, loss=loss_val)
+                
+                if n_iter >= EVALUATE_EVERY:
+                    n_iter = 0
+                    model.eval() # set model to evaluation mode
+                    with torch.no_grad():
+                        loss_num, loss_den = 0., 0.
+                        for t, (x, y) in enumerate(loader_val):
+                            x = x.to(device=device)  # move to device, e.g. GPU
+                            y = y.to(device=device)
+                            scores = model(x)
+                            loss = criterion(scores, y)
+                            loss_num += loss.item()
+                            loss_den += x.size(0)
+                        loss_val = loss_num/loss_den
+                        if loss_val < best_val_loss:
+                            best_val_loss = loss_val
+                            torch.save(model, SAVE_MODEL_PATH)
+                            writer.add_scalar('dev/BCE', best_val_loss, walltime)
+                            print('Save best model at iteration {}. Dev loss: {}'.format(walltime, best_val_loss))                    
         
+        print('Epoch %d, loss = %.4f' % (epoch, loss.item()))
+                
         model.eval() # set model to evaluation mode
         with torch.no_grad():
             num_correct = 0
@@ -104,21 +154,49 @@ def main():
                 num_correct += (preds==y).sum()
                 num_samples += preds.size(0)
             acc = float(num_correct) / num_samples
-            print('Got accuracy %f: %d / %d on training set' % (acc, num_correct, num_samples))  
+            print('Got accuracy %f: %d / %d on training set' % (acc, num_correct, num_samples))
+                        
             num_correct = 0
             num_samples = 0
-            for x, y in loader_val:
+            y_prob = np.zeros(val_dataset.__len__())
+            y_val = np.zeros(val_dataset.__len__())
+            for t, (x, y) in enumerate(loader_val):
                 x = x.to(device=device)  # move to device, e.g. GPU
                 y = y.to(device=device)
                 scores = model(x)
+                y_prob[t*BATCH_SIZE: min((t+1)*BATCH_SIZE, val_dataset.__len__())] = torch.sigmoid(scores).cpu().numpy().flatten()
+                y_val[t*BATCH_SIZE: min((t+1)*BATCH_SIZE, val_dataset.__len__())] = y.cpu().numpy().flatten()
                 preds = (scores > 0).type(dtype)
                 preds = preds.to(device)
                 num_correct += (preds==y).sum()
-                num_samples += preds.size(0) 
+                num_samples += preds.size(0)
             acc = float(num_correct) / num_samples
             print('Got accuracy %f: %d / %d on val set' % (acc, num_correct, num_samples))
+            
+            
+            y_val = y_val
+            y_prob = y_prob
+            fpr, tpr, _ = roc_curve(y_val, y_prob)
+            roc_auc = auc(fpr, tpr)
+            
+            fig, ax = plt.subplots()
+            lw = 2
+            ax.plot(fpr, tpr, color='darkorange',
+                     lw=lw, label='ROC curve (area = %0.2f)' % roc_auc)
+            ax.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+            ax.set_xlim([0.0, 1.0])
+            ax.set_ylim([0.0, 1.05])
+            ax.set_xlabel('False Positive Rate')
+            ax.set_ylabel('True Positive Rate')
+            ax.set_title('ROC')
+            ax.legend(loc="lower right")
+
+            writer.add_figure("ROC", fig, global_step=epoch)
+            
     
         #scheduler.step()
+        
+
 
 if __name__ == "__main__":
     pass
